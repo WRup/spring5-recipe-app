58ed607208166015feddc953daf16534c6db24c0
WRup <wiktor-rup@wp.pl>
2020-03-12 19:08:45 +0100

    add new Ingredient model with OneToMany relationship

--- src/main/java/guru/springframework/model/Recipe.java
+++ src/main/java/guru/springframework/model/Recipe.java
@@ -1,6 +1,7 @@
 package guru.springframework.model;
 
 import javax.persistence.*;
+import java.util.Set;
 
 @Entity
 public class Recipe {
@@ -18,6 +19,8 @@ public class Recipe {
     private String directions;
     //TODO:: add
     //private Difficulty difficulty;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
+    private Set<Ingredient> ingredients;
 
     @Lob
     private Byte[] image;
@@ -31,6 +34,14 @@ public class Recipe {
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public Set<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(Set<Ingredient> ingredients) {
+        this.ingredients = ingredients;
     }
 
     public String getDescription() {
