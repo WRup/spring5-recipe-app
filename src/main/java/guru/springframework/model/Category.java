ef911e22dc9227415c33b1e0b055e479ab479fb0
WRup <wiktor-rup@wp.pl>
2020-03-13 09:15:44 +0100

    add Category model with @ManyToMany relationship with Recipe class

---
+++ src/main/java/guru/springframework/model/Category.java
@@ -0,0 +1,21 @@
+package guru.springframework.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToMany(mappedBy = "categories")
+    private Set<Recipe> recipes;
+
+}
