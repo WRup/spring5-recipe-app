c0d7ad7e7372991fcbb34fb0c923423627e05256
WRup <wiktor-rup@wp.pl>
2020-06-04 11:22:25 +0200

    Add Spring Data JPA queries for Category adn UnitOfMeasure

---
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -0,0 +1,35 @@
+package guru.springframework.controllers;
+
+import guru.springframework.model.Category;
+import guru.springframework.model.UnitOfMeasure;
+import guru.springframework.repositories.CategoryRepository;
+import guru.springframework.repositories.UnitOfMeasureRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
+
+@Controller
+public class IndexController {
+
+
+    private final CategoryRepository categoryRepository;
+    private final UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
+    @RequestMapping({"", "/", "/index"})
+    public String getIndexPage(){
+
+        Optional<Category> categoryOptional = categoryRepository.findByDescription("American");
+        Optional<UnitOfMeasure> unitOfMeasure = unitOfMeasureRepository.findByDescription("Teaspoon");
+
+        System.out.println("Cat id is : " + categoryOptional.get().getId());
+        System.out.println("UoM id is : " + unitOfMeasure.get().getId());
+
+        return "index";
+    }
+}
